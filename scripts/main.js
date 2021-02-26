19e2c9137027b04054832fbdad9e3f7acd8dc492
Ohjun-Kwon <dhwns1003@gmail.com>
2021-02-26 13:48:03 +0000

    soft-scroll

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,23 @@
 window.onscroll = function() {scrollFunction();}
 function scrollFunction() {
-	if (document.body.scrollTop > 250 || document.documentElement.scrollTop > 250){
+	if (document.body.scrollTop > 60 || document.documentElement.scrollTop > 60){
 		document.getElementById("head-bar").style.top = "0px";
 	}else{
 		document.getElementById("head-bar").style.top = "-60px";
 	}
 	
 }
+			$(document).ready(function(){
+				$('a[href^="#"]').on('click',function (e) {
+					e.preventDefault();
+
+					var target = this.hash;
+					var $target = $(target);
+
+					$('html, body').stop().animate({
+						'scrollTop': $target.offset().top
+					}, 900, 'swing', function () {
+						window.location.hash = target;
+					});
+				});
+			});

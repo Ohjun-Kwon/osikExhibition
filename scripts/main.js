c026024edaf3ff3942face61ce2898d3ab8d3e2d
Ohjun-Kwon <dhwns1003@gmail.com>
2021-04-01 14:06:27 +0000

    1. 소개 화면 fade in 하며 등장하게 수정 2. 년도별 프로젝트 보여주는 부분 UI 수정.

--- scripts/main.js
+++ scripts/main.js
@@ -5,19 +5,47 @@ function scrollFunction() {
 	}else{
 		document.getElementById("head-bar").style.top = "-60px";
 	}
-	
+	if (document.documentElement.scrollTop > window.innerHeight/2){
+		$("#big-content").css('opacity','1');
+		$(".introduce").css('opacity','1');
+		
+		$("#big-content").attr('class','popright_fadein');
+	}
 }
-			$(document).ready(function(){
-				$('a[href^="#"]').on('click',function (e) {
-					e.preventDefault();
 
-					var target = this.hash;
-					var $target = $(target);
+$(document).ready(function(){
+	$('a[href^="#"]').on('click',function (e) {
+		e.preventDefault();
 
-					$('html, body').stop().animate({
-						'scrollTop': $target.offset().top
-					}, 900, 'swing', function () {
-						window.location.hash = target;
-					});
-				});
-			});
+		var target = this.hash;
+		var $target = $(target);
+
+		$('html, body').stop().animate({
+			'scrollTop': $target.offset().top
+		}, 900, 'swing', function () {
+			window.location.hash = target;
+		});
+	});
+});
+
+function setDisplay(){
+    if($('input:radio[id=2019]').is(':checked')){
+        $('#page2').hide();
+        $('#page3').hide();
+		
+        $('#page1').show();
+    }
+    else if($('input:radio[id=2018]').is(':checked')){
+        $('#page1').hide();
+        $('#page3').hide();
+				
+        $('#page2').show();
+    }
+    else if($('input:radio[id=2017]').is(':checked')){
+        $('#page2').hide();
+        $('#page1').hide();
+				
+        $('#page3').show();
+    }
+}
+
